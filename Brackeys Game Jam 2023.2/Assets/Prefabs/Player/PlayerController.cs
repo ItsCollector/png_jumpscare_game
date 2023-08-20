81fea4b86c03b45fa29713d84b6d4398c04f2fa3
Evan Dilkes <evandilkes@gmail.com>
2023-08-20 19:27:18 +0100

    Added player controller

---
+++ Brackeys Game Jam 2023.2/Assets/Prefabs/Player/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float jumpHeight = 3f;
+    public float gravity = -9.81f;
+
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+	void Start()
+	{
+        controller = GetComponent<CharacterController>();
+	}
+
+	void Update()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Handles WASD movement
+        Vector3 motion = transform.right * x + transform.forward * z;
+        controller.Move(motion * speed * Time.deltaTime);
+
+        // Handles jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Handles gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
